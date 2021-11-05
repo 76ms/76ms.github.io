0da2db7fbec6210be89418de769372c931aed29a
Martin Stewart <martin.stewart.100@strath.ac.uk>
2021-11-05 11:17:04 +0000

    logo typo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="logo text-center my-5">
-        <span class="cgx-blue">&lt;</span>i<span class="cgx-white">ux</span><span class="cgx-grey">ui</span><span>mo</span><span class="cgx-blue">&gt;</span>
+        <span class="cgx-blue">&lt;</span><span class="cgx-white">ux</span><span class="cgx-grey">ui</span><span>mo</span><span class="cgx-blue">&gt;</span>
     </div>
 </template>
 
